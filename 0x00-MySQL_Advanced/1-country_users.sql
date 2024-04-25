3a65203960b184d0adc93389f3b6d6dd6f347ee4
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-25 20:45:29 +0100

    not null

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,8 +3,8 @@
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters) 
 -- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
-
-CREATE TABLE IF NOT EXISTS users(
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255),
