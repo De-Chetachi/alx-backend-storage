d9ca7f6347ac7842d671837cddd5f1aa5b18818a
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-29 01:15:23 +0100

    enum

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,11 +4,10 @@
 -- name, string (255 characters) 
 -- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
+-- DROP TABLE IF EXISTS users;
+CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255),
-  country ENUM(US, CO, TN) NOT NULL DEFAULT US,
-  PRIMARY KEY (id)
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
