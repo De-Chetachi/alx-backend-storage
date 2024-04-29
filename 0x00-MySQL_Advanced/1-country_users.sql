23db1fbe872079b65724acf21b4c642260b8c217
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-29 01:34:53 +0100

    again

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,12 +1,12 @@
 -- creates a table users following these requirements:
 -- id, integer, never null, auto increment and primary key
 -- email, string (255 characters), never null and unique
--- name, string (255 characters) 
--- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
-
+-- name, string (255 characters)
 -- DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
+
+
+CREATE TABLE IF NOT EXISTS users(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255),
   country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
