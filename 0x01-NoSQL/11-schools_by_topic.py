4895cf4c0bec417a29b560a917c84fad01d9408e
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-30 16:46:17 -0700

    ok

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-'''this module contains a  Python function that lists all documents in a collection'''
+'''this module contains a  Python function
+that lists all documents in a collection'''
 from pymongo import MongoClient
 
 
 def schools_by_topic(mongo_collection, topic):
     ''' returns the list of school having a specific topic:'''
     list_ = []
-    schools = mongo_collection.find()
+    schools = mongo_collection.find({})
     for school in schools:
-        if topic in school["topics"]
+        print(school.keys)
+        if topic in school["topics"]:
             list_.append(school)
     return list_
