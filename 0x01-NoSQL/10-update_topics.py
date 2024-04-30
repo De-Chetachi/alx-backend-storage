e8fc18e3f1c43df418ddfc8028db1eba8bc2b4d5
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-30 16:20:03 -0700

    try

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''this module contains a  Python function that lists all documents in a collection'''
+from pymongo import MongoClient
+
+def update_topics(mongo_collection, name, topics):
+    ''' changes all topics of a school document based on the name'''
+    mongo_collection.update_one({"name" : name}, {'$set': {"topics" : topics}})
