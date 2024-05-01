f35267556dd2e515912fe9f5caf3386456fdec68
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-30 18:10:53 -0700

    regular exp

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 '''this module contains a  Python function
 that lists all documents in a collection'''
-from pymongo import MongoClient
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-print(f'{collection.count_documents({})} logs')
-print("Methods:")
-get = collection.count_documents({"method": "GET"})
-post = collection.count_documents({"method": "POST"})
-put = collection.count_documents({"method": "PUT"})
-patch = collection.count_documents({"method": "PATCH"})
-delete = collection.count_documents({"method": "DELETE"})
-stat = collection.count_documents({"method": "GET", "path": "/status"})
-print(f'    method GET: {get}')
-print(f'    method POST: {post}')
-print(f'    method PUT: {put}')
-print(f'    method PATCH: {patch}')
-print(f'    method DELETE: {delete}')
-print(f'{stat} status check')
+
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    print(f'{collection.count_documents({})} logs')
+    print("Methods:")
+    get = collection.count_documents({"method": "GET"})
+    post = collection.count_documents({"method": "POST"})
+    put = collection.count_documents({"method": "PUT"})
+    patch = collection.count_documents({"method": "PATCH"})
+    delete = collection.count_documents({"method": "DELETE"})
+    stat = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f'    method GET: {get}')
+    print(f'    method POST: {post}')
+    print(f'    method PUT: {put}')
+    print(f'    method PATCH: {patch}')
+    print(f'    method DELETE: {delete}')
+    print(f'{stat} status check')
